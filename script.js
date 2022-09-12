92076b4c001e37ebfe4f8df13de98df99a04a1b8
NickySimps <nick.a.simpson@gmail.com>
2022-09-11 21:50:09 -0400

    Added measure as an object and added total beats display

--- script.js
+++ script.js
@@ -11,161 +11,171 @@ const decBarsBtn = document.querySelector(".decrease-bar-length");
 const incBarsBtn = document.querySelector(".increase-bar-length");
 const currentBars = document.querySelector(".bars-length");
 const barDisplay = document.querySelector(".bar-display-container");
+const totalBeatsDisplay = document.querySelector(".total-beats-display");
 const newMeasureBar = `<div class="measure-bar"></div>`;
 const newMeasureBarSegment = `<div class="measure-bar-segment"></div>`;
 const measureBarSegment = document.querySelector(".measure-bar-segment");
 
+totalBeatsDisplay.textContent = test();
+
+const measure = {
+  beats: 4,
+  selected: false,
+  makeMeasure: function () {
+    barDisplay.innerHTML += newMeasureBar;
+    this.beats = selectedBarBeats;
+    for (i = 0; i < this.beats; i++) {
+      let lastBlock = barDisplay.lastElementChild;
+      lastBlock.innerHTML += newMeasureBarSegment;
+    }
+  },
+  removeMeasure: function () {
+    let lastBlock = barDisplay.lastElementChild;
+    barDisplay.removeChild(lastBlock);
+  },
+};
+
 const click1 = new Audio("Click1.mp3");
-const click2 = new Audio('Click2.mp3');
-const crank1 = new Audio('Crank1.mp3');
-const crank2 = new Audio('Crank2.mp3');
-
-
+const click2 = new Audio("Click2.mp3");
+const crank1 = new Audio("Crank1.mp3");
+const crank2 = new Audio("Crank2.mp3");
 
 let currentTempo = 120;
-let selectedBarBeats = 5;
-let amountOfBars = 4;
+let selectedBarBeats = 0;
+let amountOfBars = 1;
 
 window.onload = init();
 
 function init() {
-    drawBars();
-    drawBeats();
-
+  drawBars();
+  drawBeats();
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~TEMPO CONTROLLER~~~~~~~~~~~~~~~~~~~~~~~~~
 
 decTempoBtn.addEventListener("click", () => {
-
-    if (currentTempo > 20) {
-        currentTempo--;
-    }
-    updateTempo();
+  if (currentTempo > 20) {
+    currentTempo--;
+  }
+  updateTempo();
 });
 incTempoBtn.addEventListener("click", () => {
-    if (currentTempo < 300) {
-        currentTempo++;
-    }
-    updateTempo();
+  if (currentTempo < 300) {
+    currentTempo++;
+  }
+  updateTempo();
 });
 tempoSlider.addEventListener("input", () => {
-    currentTempo = tempoSlider.value;
-    updateTempo();
+  currentTempo = tempoSlider.value;
+  updateTempo();
 });
 
 function updateTempo() {
-    tempo.textContent = currentTempo;
-    tempoSlider.value = currentTempo;
-    if (currentTempo < 60) {
-        tempoText.textContent = 'very slow'
-    } else if (currentTempo > 60 && currentTempo < 90) {
-        tempoText.textContent = 'slow'
-    } else if (currentTempo > 90 && currentTempo < 120) {
-        tempoText.textContent = 'moderate'
-    } else if (currentTempo > 120 && currentTempo < 160) {
-        tempoText.textContent = 'kinda fast'
-    } else if (currentTempo > 160 && currentTempo < 200) {
-        tempoText.textContent = 'fast'
-    } else if (currentTempo > 200 && currentTempo < 230) {
-        tempoText.textContent = 'very fast'
-    } else if (currentTempo > 230 && currentTempo < 260) {
-        tempoText.textContent = 'extremely fast'
-    } else if (currentTempo > 260) {
-        tempoText.textContent = 'woah nelly'
-    }
+  tempo.textContent = currentTempo;
+  tempoSlider.value = currentTempo;
+  if (currentTempo < 60) {
+    tempoText.textContent = "very slow";
+  } else if (currentTempo > 60 && currentTempo < 90) {
+    tempoText.textContent = "slow";
+  } else if (currentTempo > 90 && currentTempo < 120) {
+    tempoText.textContent = "moderate";
+  } else if (currentTempo > 120 && currentTempo < 160) {
+    tempoText.textContent = "kinda fast";
+  } else if (currentTempo > 160 && currentTempo < 200) {
+    tempoText.textContent = "fast";
+  } else if (currentTempo > 200 && currentTempo < 230) {
+    tempoText.textContent = "very fast";
+  } else if (currentTempo > 230 && currentTempo < 260) {
+    tempoText.textContent = "extremely fast";
+  } else if (currentTempo > 260) {
+    tempoText.textContent = "woah nelly";
+  }
 }
 
 //~~~~~~~~~~START-STOP BUTTON~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 startStopBtn.addEventListener("click", () => {
-
-    if (startStopBtn.textContent === "START") {
-        startStopBtn.textContent = "STOP";
-    } else {
-        startStopBtn.textContent = "START";
-
-    }
-    playClick();
-
-
+  if (startStopBtn.textContent === "START") {
+    startStopBtn.textContent = "STOP";
+  } else {
+    startStopBtn.textContent = "START";
+  }
+  playClick();
 });
 
 //~~~~~~~~~~~BEATS CONTROLLER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 decBeatsBtn.addEventListener("click", () => {
-    if (selectedBarBeats > 1) {
-        selectedBarBeats--;
-        let measureBar = document.querySelector(".measure-bar");
-        let lastSegment = measureBar.lastElementChild;
-        measureBar.removeChild(lastSegment);
-    }
-    updateBeats();
+  if (selectedBarBeats > 1) {
+    selectedBarBeats--;
+    let lastBlock = barDisplay.lastElementChild;
+    let lastSegment = lastBlock.lastElementChild;
+    lastBlock.removeChild(lastSegment);
+  }
+  updateBeats();
 });
 
 incBeatsBtn.addEventListener("click", () => {
-    if (selectedBarBeats < 19) {
-        selectedBarBeats++;
-        let measureBar = document.querySelector(".measure-bar");
-        measureBar.innerHTML += newMeasureBarSegment;
-    }
-    updateBeats();
+  if (selectedBarBeats < 19) {
+    selectedBarBeats++;
+    let lastBlock = barDisplay.lastElementChild;
+    lastBlock.innerHTML += newMeasureBarSegment;
+  }
+  updateBeats();
 });
 function updateBeats() {
-    currentBeats.textContent = selectedBarBeats;
+  let lastBlock = barDisplay.lastElementChild;
+  currentBeats.textContent = lastBlock.getElementsByTagName("*").length;
+  totalBeatsDisplay.innerHTML += 99;
 }
 
 //~~~~~~~~~~~~~~BARS CONTROLLER~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 decBarsBtn.addEventListener("click", () => {
-    if (amountOfBars > 1) {
-        amountOfBars--;
-        let lastBlock = barDisplay.lastElementChild;
-        barDisplay.removeChild(lastBlock);
-    }
-    updateBars();
+  if (amountOfBars > 1) {
+    amountOfBars--;
+    measure.removeMeasure();
+  }
+  updateBars();
+  updateBeats();
 });
 
 incBarsBtn.addEventListener("click", () => {
-    amountOfBars++;
-    barDisplay.innerHTML += newMeasureBar;
-    updateBars();
+  amountOfBars++;
+  measure.makeMeasure();
+  updateBars();
 });
 
 function updateBars() {
-    currentBars.textContent = amountOfBars;
+  currentBars.textContent = amountOfBars;
 }
 
 //~~~~~~~~~~~~~~~~~~~FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 function drawBars() {
-    updateBars();
-    for (let i = 0; i < currentBars.textContent; i++) {
-        barDisplay.innerHTML += newMeasureBar;
-    }
-
+  updateBars();
+  for (let i = 0; i < currentBars.textContent; i++) {
+    barDisplay.innerHTML += newMeasureBar;
+  }
 }
 function drawBeats() {
-    let measureBar = document.querySelector('.measure-bar');
-    updateBeats();
-    for (let i = 0; i < currentBeats.textContent; i++) {
-        measureBar.innerHTML += newMeasureBarSegment;
-    }
-
+  let measureBar = document.querySelector(".measure-bar");
+  updateBeats();
+  for (let i = 0; i < currentBeats.textContent; i++) {
+    measureBar.innerHTML += newMeasureBarSegment;
+  }
+}
+function test() {
+  let allMeasures = document.querySelectorAll(".measure-bar");
+  return "Total Beats:" + 99;
 }
 
 function playClick() {
-    setTimeout(() => {
-        click1.play();
-    }, (60 / tempoSlider.value) * 500);
-    setTimeout(() => {
-        click2.play();
-        playClick();
-
-    }, (60 / tempoSlider.value) * 1000);
-
+  setTimeout(() => {
+    click1.play();
+  }, (60 / tempoSlider.value) * 500);
+  setTimeout(() => {
+    click2.play();
+    playClick();
+  }, (60 / tempoSlider.value) * 1000);
 }
-
-
-
-
